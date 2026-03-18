cd40300524d253a1bf6225c683b59cc32041609c
TheLis3k <ksawery2k19@gmail.com>
2026-03-18 17:20:47 +0100

    ai not working

---
+++ lua/config/ai.lua
@@ -0,0 +1,16 @@
+local function open_ai(role)
+    vim.cmd("vsplit")
+    vim.cmd("term aichat -r " .. role)
+    vim.cmd("startinsert")
+end
+local function send_visual_to_ai()
+    vim.cmd('normal! "ay')
+    local selection = vim.fn.getreg('a')
+    vim.cmd("wincmd l")
+    vim.api.nvim_chan_send(vim.b.terminal_job_id, selection .. "\n")
+    vim.cmd("wincmd h")
+end
+
+vim.keymap.set("n", "<leader>af", function() open_ai("fast") end, { desc = "AI Fast (Flash)" })
+vim.keymap.set("n", "<leader>as", function() open_ai("smart") end, { desc = "AI Smart (Pro)" })
+vim.keymap.set("v", "<leader>v", send_visual_to_ai, { desc = "Send to AI" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,3 +14,5 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.splitright = true
+vim.opt.splitbelow = true
